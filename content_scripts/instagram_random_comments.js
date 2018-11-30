3d76875cee80ddcc37485bfc1141609deaf802e7
gonzalejandro <aege21@gmail.com>
2018-11-30 13:49:56 -0300

    Fixed synchronization problems between load and choose comments, saving current state in content script. Also added animations and color addition to choose comment process

--- content_scripts/instagram_random_comments.js
+++ content_scripts/instagram_random_comments.js
@@ -4,62 +4,59 @@
 	}
 
 	window.hasRun = true;
+	window.popupState = "unloaded";
 
 	var intervalClickerId;
 	let loadingGifUrl;
-	let commentsList = [];
+	let totalComments = 0;
 
 	/**
 	 * Extracts the comment of the given index from the Instagram
 	 * post HTML
 	*/
 	function commentGetter(index) {
-		const fixedIndex = index + 2;
-		const base = "article > div:nth-child(3) > div:nth-child(3) > ul:nth-child(1) > li:nth-child(" + fixedIndex.toString() + ")";
+		index += 2;
+		const base = "article > div:nth-child(3) > div:nth-child(3) > ul:nth-child(1) > li:nth-child(" + index.toString() + ")";
 		const stringComment = document.querySelector(base + "> div > div > div > span").innerHTML;
 		const stringAuthor = document.querySelector(base + " > div > div > div > h3 > a").innerHTML;
 		return {author: stringAuthor, comment: stringComment};
 	}
 
 
-	function insertLoadingGif(node) {
-		let loadingIcon = document.createElement("img");
-		loadingIcon.setAttribute("src", loadingGifUrl);
-		loadingIcon.className = "loading-gif";
-		node.parentNode.insertBefore(loadingIcon, node.nextSibling);
-	}
-
-
-	function removeLoadingGif(node) {
-		node.nextSibling.style.display = "None";
-	}
-
-
-	function toggleComentsLoading() {
+	function showComentsLoadingIcon() {
 		if (!window.displayState) {
- 			window.displayState = "block";
+			window.displayState = "block";
 		}
 
-		let comments =  document.querySelector("article > div:nth-child(3) > div:nth-child(3) > ul:nth-child(1)");
 		if (window.displayState === "block") {
+			let comments =  document.querySelector("article > div:nth-child(3) > div:nth-child(3) > ul:nth-child(1)");
 			comments.style.display = "none";
 			window.displayState = "none";
-			insertLoadingGif(comments);
 			
-		} else {
-			comments.style.display = "block";
-			window.displayState = "block";
-			removeLoadingGif(comments);
+			let loadingIcon = document.createElement("img");
+			loadingIcon.setAttribute("src", loadingGifUrl);
+			loadingIcon.className = "loading-gif";
+			comments.parentNode.insertBefore(loadingIcon, comments.nextSibling);
 		}
 	}	
 
 
+	function hideCommentsLoadingIcon() {
+		if (window.displayState === "none") {
+			let comments =  document.querySelector("article > div:nth-child(3) > div:nth-child(3) > ul:nth-child(1)");
+			comments.style.display = "block";
+			comments.nextSibling.style.display = "none";
 
-	function notifyLoad() {
+			window.displayState = "block";	
+			window.popupState = "loaded";		
+		}
+	}
+
+	function notifyLoad(totalComments) {
 		browser.runtime.sendMessage({
-			isLoaded: true
-		});	
-		console.log("sent message");
+			isLoaded: true,
+			totalComments: totalComments
+		});
 	}
 
 	/**
@@ -68,19 +65,50 @@
 	*/
 	function clicker() {
 		try {
-			document.querySelector("article > div:nth-child(3) > div:nth-child(3) > ul:nth-child(1) > li:nth-child(2) > button:nth-child(1)").click();
-			if (!window.isLoading) {
-				toggleComentsLoading();
-				window.isLoading = true;
-			}
+			document.querySelector("article > div:nth-child(3) > div:nth-child(3) > ul:nth-child(1) > li:nth-child(2) > button:nth-child(1)").click();	
+			showComentsLoadingIcon();
+			
 		} catch (err) {
-			console.log("Finished comments loading");
 			clearInterval(intervalClickerId);
-			toggleComentsLoading();
 
-			notifyLoad();
+			
+			hideCommentsLoadingIcon();
+
+			while (true) {
+				try {
+					commentGetter(totalComments);
+					totalComments++;
+				} catch(err) {
+					break;
+				}
+			}
+			console.log(totalComments);
+			notifyLoad(totalComments);
 		}
 	};	
+
+
+	function displayComment(commentId) {
+		commentId++;
+		if (!window.highlighted) {
+			window.highlighted = commentId;
+		}
+
+		const base = "article > div:nth-child(3) > div:nth-child(3) > ul:nth-child(1) > li:nth-child(";
+
+		let highlightedComment = document.querySelector(base + window.highlighted.toString() + ")");
+		highlightedComment.style.backgroundColor = "";
+
+		window.highlighted = commentId;
+
+		let comment = document.querySelector(base + commentId.toString() + ")");
+		comment.style.backgroundColor = "yellow";
+		comment.scrollIntoView({
+			behavior: 'smooth'
+		});
+		document.querySelector("main").scrollIntoView();
+
+	}
 
 
 	/**
@@ -88,12 +116,19 @@
  	 * using the beacon output
 	*/
 	browser.runtime.onMessage.addListener((message) => {
-		console.log(message);
-		if (message.command === "choose") {
+		if (message.command === "load") {
 			const  timeInterval = 500;
 			loadingGifUrl = message.loadingUrl;
 			intervalClickerId = window.setInterval(clicker, timeInterval);
-			console.log(commentsList);
+
+		} else if (message.command === "choose") {
+			displayComment(message.comment);
+		
+		} else if (message.command === "state") {
+			browser.runtime.sendMessage({
+				state: window.popupState,
+				totalComments: totalComments
+			});
 		}
 	});
 })();
--- popup/choose_comment.js
+++ popup/choose_comment.js
@@ -1,74 +1,140 @@
 
 
-function doNothing() {
-	console.log("doing nothing");
+let totalComments;
+
+/**
+ * General report execute error function
+*/
+function reportExecuteScriptError(error) {
+  	document.querySelector("#popup-content").classList.add("hidden");
+  	document.querySelector("#error-content").classList.remove("hidden");
+	console.error(error);
 }
 
-function listenForClicks() {
-	document.querySelector("#chooser").addEventListener("click", (e) => {
 
-		/**
-		 * Sends the message to start loading comments. Once the message is sent
-		 * disables the extension button.
-		 */
+/**
+ * Loading Function's section
+*/
+function listenClickToLoad() {
+	
+	function sendLoadingMessage(e) {
 		function loadComments(tabs) {
 			let url = browser.extension.getURL("gif/loading.gif");
 			browser.tabs.sendMessage(tabs[0].id, {
-				command: "choose",
+				command: "load",
 				loadingUrl: url
-			});
-
+			}).then(listenForLoaded);
+			
 			let chooser = document.querySelector("#chooser");
-			chooser.removeEventListener("click", listenForClicks);
-			chooser.addEventListener("click", doNothing);
+			chooser.removeEventListener("click", sendLoadingMessage);
 			chooser.classList.remove("chooser");
 			chooser.classList.add("chooser-blocked");
 		}
+
 
 		function reportError(error) {
 			console.error(error);
 		}
 
+
 		browser.tabs.query({active: true, currentWindow: true})
 			.then(loadComments)
 			.catch(reportError);
-	});
+	}
+
+	document.querySelector("#chooser").addEventListener("click", sendLoadingMessage);
 }
 
-function reportExecuteScriptError(error) {
-  	document.querySelector("#popup-content").classList.add("hidden");
-  	document.querySelector("#error-content").classList.remove("hidden");
-	console.error(`Failed to execute beastify content script: ${error.message}`);
+
+function listenForLoaded() {
+	
+	function reportError(error) {
+		console.error(error);
+	}
+
+
+	function handleLoaded(message) {
+		totalComments = message.totalComments;
+		listenClickToChoose();
+	}	
+
+
+	browser.runtime.onMessage.addListener(handleLoaded);
 }
 
 
 /**
- * Given a string representing an url, gets its JSON content
-*/
-function jsonFromUrl(url){
-    var request = new XMLHttpRequest(); // a new request
-    request.open("GET", url, false);
-    request.send(null);
-    return JSON.parse(request.responseText);          
+ * Choose Function's section
+ */
+function randInt(min, max) {
+	return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-
-function listenForLoading() {
-	function handleLoading() {
-		let chooser = document.querySelector("#chooser");
+function listenClickToChoose() {
+	let chooser = document.querySelector("#chooser");
+	if (chooser.innerHTML !== "Sortear") {
 		chooser.innerHTML = "Sortear";
 		chooser.classList.remove("chooser-blocked");
-		chooser.classList.add("chooser");	
-	}	
+		chooser.classList.add("chooser");
+	}
 
-	browser.runtime.onMessage.addListener(handleLoading);
+	function sendChoosenMessage(e) {
+
+		function chooseComment(tabs) {
+			const commentId = randInt(0, totalComments);
+			browser.tabs.sendMessage(tabs[0].id, {
+				command: "choose",
+				comment: commentId
+			});
+		}
+
+		browser.tabs.query({active: true, currentWindow: true})
+			.then(chooseComment);
+	} 
+
+	document.querySelector("#chooser").addEventListener("click", sendChoosenMessage);	
 }
 
 
+function listenForState() {
+	function reportError(error) {
+		console.error(error);
+	}
+
+
+	function handleState(message) {
+		if (message.state === "unloaded") {
+			listenClickToLoad();
+		} else if (message.state === "loaded") {
+			totalComments = message.totalComments;
+			listenClickToChoose();
+		}
+	}
+
+	browser.runtime.onMessage.addListener(handleState);
+}
+
+function askForState() {
+	function reportError(error) {
+		console.error(error);
+	}
+
+	function sendMessage(tabs)  {
+		const commentId = randInt(0, totalComments-1);
+		browser.tabs.sendMessage(tabs[0].id, {
+			command: "state",
+		});
+	}
+	
+
+	browser.tabs.query({active: true, currentWindow: true})
+			.then(sendMessage)
+			.then(listenForState)
+			.catch(reportError);
+}
 
 browser.tabs.executeScript({file: "/content_scripts/instagram_random_comments.js"})
-	.then(listenForClicks)
-	.then(listenForLoading)
+	.then(askForState)
 	.catch(reportExecuteScriptError);
 
 
